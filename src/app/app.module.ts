1cfec0a0d5c32e0e6e696e7825fc847ec1b6e2bd
edsonnsousa <edsonn.borgess@gmail.com>
2019-07-15 23:21:23 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { ApiService } from './api.service';
 import { AppComponent } from './app.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ListComponent } from './list.component';
+import { LoginComponent } from './login.component';
+import { SignupComponent } from './signup.component';
+
+import { AuthService, AuthInterceptor, AuthGuard } from './auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    AuthService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
